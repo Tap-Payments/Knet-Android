75baf82816dc0926371a728a1d58816f4dec1774
Aslm <130293799+aslm-android@users.noreply.github.com>
2024-01-25 04:28:48 -0300

    added proguard rules

--- knet-sdk/src/main/java/company/tap/tapWebForm/open/web_wrapper/TapKnetPay.kt
+++ knet-sdk/src/main/java/company/tap/tapWebForm/open/web_wrapper/TapKnetPay.kt
@@ -429,7 +429,6 @@ class TapKnetPay : LinearLayout {
 
 
     override fun onDetachedFromWindow() {
-        Log.e("detached","here")
         knetWebView.destroy()
         super.onDetachedFromWindow()
     }
